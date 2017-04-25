5fd66545ab9e1066c69506be5640176e367af47f
Ming Xu <mrxuming@gmail.com>
2017-04-25 07:23:43 +0000

    mission complete

--- server.js
+++ server.js
@@ -2,8 +2,24 @@
 let express = require("express");
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+'August', 'September', 'October', 'November', 'December'];
+app.get('/:timeString', (req, res) => {
+    let result = {
+        unix: null,
+        natural: null
+    };
+    let msec = Number(req.params.timeString);
+    let date;
+    if (msec)
+        date = new Date(msec * 1000);
+    else
+        date = new Date(req.params.timeString);
+    if (date && date.getTime()){
+        result.natural = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+        result.unix = Date.parse(result.natural) / 1000;
+    }
+    res.json(result);
 });
 
 app.listen(8080, () => console.log('express app is running!'));
